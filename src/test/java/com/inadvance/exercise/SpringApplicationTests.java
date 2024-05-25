a5f9249a8de25e45bf228be17b447e76db4d4526
Daniel <ri.da.igle.es@gmail.com>
2024-05-25 15:01:53 -0500

    feature:Initial Commit

---
+++ src/test/java/com/inadvance/exercise/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inadvance.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
